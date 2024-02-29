906f1e82477039b6d6391cb4180c4871a3f192b3
Simeï Steiner <simei.steiner@ferag.com>
2024-02-29 11:54:41 +0100

    feat: add DB

---
+++ DataManager/Migrations/20240229104522_Init.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Words",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Word = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Words", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Words");
+        }
+    }
+}
---
+++ DataManager/TextDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataManager.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DataManager;
+public class TextDbContext : 
+    DbContext
+{
+    private readonly string _connectionString;
+    public TextDbContext(IConfiguration config)
+    {
+        var relativePath = config.GetConnectionString("LotteryDraws") ?? throw new NullReferenceException();
+        _connectionString = Path.GetFullPath(relativePath);
+    }
+
+    public DbSet<Words> Words { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite($"Data Source={_connectionString}");
+    }
+}
