f519e006ff37e1f2b3e331f5b3f97ec874a243b5
mschiltknecht <marco.schiltknecht@m-f.ch>
2024-02-29 12:03:52 +0100

    added event class

---
+++ TextIO/Event.cs
@@ -0,0 +1,17 @@
+﻿namespace TextIO;
+
+public class Event<T>
+{
+    public string Type { get; set; }
+    public DateTime TimeStamp { get; }
+    public string Subject { get; set; }
+    public T Data { get; set; }
+
+    Event(T data, string type, string subject)
+    {
+        Type = type;
+        TimeStamp = DateTime.Now;
+        Subject = subject;
+        Data = data;
+    }
+}
